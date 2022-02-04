2f101e9c49141902e84420d52e2da4439d5286df
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-03 19:21:50 -0500

    Create welcomescreen.cs

---
+++ welcomescreen.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Amazing Accounting Application!\nYou can \"A count\" on us!");
+        Console.ReadLine();//this keeps the window open until we close it, otherwise it closes right away and we can't see "Hello, World!" in the console
+        }
+    }

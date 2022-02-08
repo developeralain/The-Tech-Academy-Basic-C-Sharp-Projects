a0919b0e36c732155be5e3c8e741539fc9a5b005
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-08 13:51:07 -0500

    MethodSubmissionAssignment

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math time! Enter any whole number: ");
+            string xString = Console.ReadLine();
+            int x = Convert.ToInt32(xString);
+
+            Console.WriteLine("Optional - enter a second number (press 'Enter' to skip): ");
+            
+            string yString = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(yString))//checks to see if the user chose not to enter a second number: only executes if they DID enter a second number i.e. not true that yString is empty/null
+            {
+                int y = Convert.ToInt32(yString);
+                int AddFive = new math1().AddNum(x, y);//calls method AddFive in object math1 and passes in userInt; method returns an int that is stored in AddFive variable
+                Console.WriteLine("MATH RESULTS: " + AddFive);
+                Console.Read();
+            }
+            else//only executes if there is no second number entered by the user i.e. only executes if user entered one number and skipped the optional.
+            {
+
+                int AddFive = new math1().AddNum(x);//calls method AddFive in object math1 and passes in userInt; method returns an int that is stored in AddFive variable
+                Console.WriteLine("MATH RESULTS: " + AddFive);
+                Console.Read();
+            }
+
+        }
+        
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/math1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class math1
+    {
+        public int AddNum(int x, int y = 985497685)//the default value is unlikely to be entered by a user, hence no input means y=985497685 which means else block runs and ignores y
+        {
+            if (y != 985497685)//meaning if user enters any second value (i.e. y = any number other than its default value)
+            {
+                int sum = x + y;
+                return sum;
+            }
+            else//so if y is its default value, ignore y and only compute with single user input x 
+            {
+                int sum = x + 5;
+                return sum;
+            }
+
+        }
+    }
+}

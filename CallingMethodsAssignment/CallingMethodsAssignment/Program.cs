49a90a7df7bfd4c6edb2b744ce658cf43350f9e7
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-07 20:11:14 -0500

    CallingMethodsAssignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number. I'll show you some legendary math operations in just a moment.");
+            string userInput = Console.ReadLine();
+            int userInt = Convert.ToInt32(userInput);
+            int AddFive = new math1().AddNum(userInt);
+            int SubFive = new math1().SubNum(userInt);
+            int MultiFive = new math1().MultiNum(userInt);
+            Console.WriteLine("Voila! We turned " + userInput + " into :" + AddFive + ", " + SubFive + ", " + MultiFive);
+            Console.Read();
+
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/math1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class math1
+    {
+        public int AddNum(int x)
+        {
+            int y = x + 5;
+            return y;
+        }
+        public int SubNum(int x)
+        {
+            int y = x - 5;
+            return y;
+        }
+        public int MultiNum(int x)
+        {
+            int y = x * 5;
+            return y;
+        }
+    }
+}

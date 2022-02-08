bad7cbfc342fdd6d2857f9dd7e0ec7bbe1895d78
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-08 15:11:00 -0500

    mathClassAssignment

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math1 mathMagic = new math1();
+            mathMagic.mathPrint(3, 4);//runs void method in class object 
+            Console.ReadLine();//keeps console open
+
+            mathMagic.mathPrint(x: 3, y: 4);//also runs method but specifies in arguements passed the parameters by name
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/math1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class math1
+    {
+        public void mathPrint(int x, int y)//takes in x and y inputs from user (two separate numerical inputs)
+        {
+            //does math with x but does not display it, displays y (second user input) on screen
+            int sum = x + 1111 / 2;
+            Console.WriteLine("Here's you second number back: {0}", y);
+            Console.WriteLine(sum);//verifies that math operation was performed correctly
+            Console.Read();
+        }
+    }
+}

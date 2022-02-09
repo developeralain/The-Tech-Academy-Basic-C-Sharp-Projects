c0bc0d134e44bfa8d79f4005c4ea316731307c04
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-08 20:06:00 -0500

    MethodsAndObjectsAssignment

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()//void (doesn't return anything) method that prints First and Last Name in console
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };//instantiates class object employee and initializes it with values for its properties
+            employee.SayName();//calling Employee superclass method 'SayName' on employee object
+            
+
+
+        }
+    }
+}

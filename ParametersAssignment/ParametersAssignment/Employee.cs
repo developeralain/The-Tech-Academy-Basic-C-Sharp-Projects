7a1e874e05ab0f4af4b8d4b5c9eda35275938671
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-10 16:54:18 -0500

    Parameters Assignment

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T> : Person//this class inherits from abstract class Person
+    {
+        public List<T> Things { get; set; }
+        public override void SayName()//override keyword allows us to add in our own implementation details to this abstract method we are inheriting
+        {
+            Console.WriteLine(FirstName + " " + LastName);//prints First and Last Name properties, which aren't explicitly written here but inherited from Person, to console
+        }
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Billy = new Employee<string>();//instantiate class object of Employee class as 'Billy' of 'string' datatype 
+            Billy.Things = new List<string>() { "Chicken egg", "Pretzel sandwich", "Old widget", "Neutral pacifier" } ;//instantiated the Things list property as a list of strings and initialized it with values
+
+            Employee<int> PointDexter = new Employee<int>();//instantiate class object of Employee class as 'PointDexter' of 'integer' datatype 
+            PointDexter.Things = new List<int>() { 2,3,52,3,111 };//instantiated the Things list property as a list of integers and initialized it with numerical values
+
+            foreach (string thing in Billy.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (int thing in PointDexter.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.Read();
+        }
+    }
+}

30b9466a90b543434eb8e07d3de26e468b5142a7
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-11 12:10:12 -0500

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number//publicly accessible struct called Number
+    {
+        public decimal Amount { get; set; }//a decimal property for our struct, called Amount
+
+
+    }
+}

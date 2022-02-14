cc640f853e89269075e836c5d35e818aace7e012
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-14 11:16:11 -0500

    ConstructorSubmissionAssignment

---
+++ ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssignment
+{
+    public class Dog
+    {
+        public Dog(string prefix, string name)//constructor constructs a pet name upon instantiation of class object (e.g. Mr Scruffy)
+        {
+            Prefix = prefix;
+            Name = name;
+        }
+        public Dog(string name) : this("Mr. ", name) //here we have name as the 2nd parameter, but for the 1st parameter he have set prefix to "Mr. " by default
+        {
+        }
+        public string Prefix { get; set; }//prefix property
+        public string Name { get; set; }//nbame property
+    }
+}
---
+++ ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Alain";//declares a constant 
+            Console.WriteLine(myName);
+            var favFruit = "Apple";
+            Console.WriteLine(favFruit);
+
+            Dog dog = new Dog("Pebbles");//instantiates new Dog object as dog and passes in 1 parameter only, using chained constructors 
+            Console.WriteLine(dog.Prefix + dog.Name); //OUTPUT = "Mr. Pebbles"
+
+
+            Dog doggie = new Dog("Mr. ", "Doggles");//instantiates new Dog object as doggie and passes in 2 parameters
+            Console.WriteLine(doggie.Prefix + doggie.Name);//OUTPUT = "Mr. Doggles"
+            Console.Read();
+
+        }
+    }
+}

ff6bf16ac1d987c2b7abd16f61b0b596382618e8
Alain Lipowicz <alainsoftwaredev@gmail.com>
2022-02-23 16:43:01 -0500

    CodeFirstDb

---
+++ CodeFirst2/CodeFirst2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill", StudentID = 123, Height = 123, Weight = 173 };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+            }
+            Console.WriteLine("Done.");
+            Console.Read();
+        }
+    }
+}
---
+++ CodeFirst2/CodeFirst2/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst2
+{
+    internal class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
+/*
+  It derives from DBContext class and exposes DbSet properties for the types that you want to be part of the model, e.g. Student and Grade classes in this case. 
+The DbSet is a collection of entity classes (aka entity set), so we have given the property name as the plural of entity name like Students and Grades.
+ */
